9947e460fc0bfea48b0bfc92931f2976f2ea1e20
Deepak Kumar <dk1184366@gmail.com>
2025-11-07 09:52:27 +0530

    commit

---
+++ Class Pratice Js/For Lop/Substrings.js
@@ -0,0 +1,11 @@
+function allSubstrings(str) {
+  let subs = [];
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i + 1; j <= str.length; j++) {
+      subs.push(str.slice(i, j));
+    }
+  }
+  return subs;
+}
+
+console.log(allSubstrings("abc"));
