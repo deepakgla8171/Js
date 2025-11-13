f849aeaa94ee0a021b68769270304855afac03e5
Deepak Kumar <dk1184366@gmail.com>
2025-11-13 14:50:45 +0530

    commit

---
+++ Class Pratice Js/String/noRepating.js
@@ -0,0 +1,7 @@
+function firstNonRepeat(str) {
+  for (let ch of str) {
+    if (str.indexOf(ch) === str.lastIndexOf(ch)) return ch;
+  }
+  return null;
+}
+console.log(firstNonRepeat("aabbcdee")); 
