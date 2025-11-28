2823ceb0d9d365105ae409b55c9e8bbc14ae5e71
Deepak Kumar <dk1184366@gmail.com>
2025-11-28 09:24:49 +0530

    commit

---
+++ Class Pratice Js/Promise/Finally.js
@@ -0,0 +1,7 @@
+let p = new Promise((resolve, reject) => {
+    reject("Error!");
+});
+
+p.then(res => console.log(res))
+ .catch(err => console.log(err))
+ .finally(() => console.log("Done running promise"));
