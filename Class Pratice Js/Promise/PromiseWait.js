03137642c55e2bc6d8d3dc8d1158db46b0c1c7d3
Deepak Kumar <dk1184366@gmail.com>
2026-01-21 21:37:37 +0530

    commit

--- Class Pratice Js/Promise/PromiseWait.js
+++ Class Pratice Js/Promise/PromiseWait.js
@@ -1,6 +1,6 @@
 function fetchUser() {
     return new Promise(resolve => {
-        setTimeout(() => resolve("User Loaded"), 4000);
+        setTimeout(() => resolve("User Loaded"), 3000);
     });
 }
 
