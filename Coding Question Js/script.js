1b2c26784f914a9dac698c7872570259ff44a5a0
Deepak Kumar <dk1184366@gmail.com>
2026-01-31 11:58:34 +0530

    commit

--- Coding Question Js/script.js
+++ Coding Question Js/script.js
@@ -12,7 +12,7 @@ function analyzeScores(scoresArray) {
        sum  = sum+score;
        if(score >= 90){
         excellentCount++;
-       }else if(score >= 40){
+       }else if(score >= 20){
         passCount++;
        }else{
         failCount++;
