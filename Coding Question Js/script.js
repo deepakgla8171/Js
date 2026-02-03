7685631a89563680df534d0a582c6b4e86969e85
Deepak Kumar <dk1184366@gmail.com>
2026-02-03 21:29:14 +0530

    commit

--- Coding Question Js/script.js
+++ Coding Question Js/script.js
@@ -3,7 +3,7 @@ function analyzeScores(scoresArray) {
    let passCount =0;
    let failCount =0; 
    let total = 0;
-   let sum =1;
+   let sum =0;
    let average =0;
    for(let i=0; i<scoresArray; i++){
       let score = Number(scoresArray[i]);
