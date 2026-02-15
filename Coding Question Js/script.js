f6372d85041a94482d980dea0c829e2b22575bfd
Deepak Kumar <dk1184366@gmail.com>
2026-02-15 20:35:16 +0530

    commit

--- Coding Question Js/script.js
+++ Coding Question Js/script.js
@@ -4,7 +4,7 @@ function analyzeScores(scoresArray) {
    let failCount =0; 
    let total = 1;
    let sum =0;
-   let average =1;
+   let average =2;
    for(let i=0; i<scoresArray; i++){
       let score = Number(scoresArray[i]);
       if(isNaN(Number)) 
