0342cbb552a910ed89d1a6f7ae95e585b79d0254
Deepak Kumar <dk1184366@gmail.com>
2026-03-18 21:20:54 +0530

    commit

--- Class Pratice Js/Math/clamMinMax.js
+++ Class Pratice Js/Math/clamMinMax.js
@@ -2,5 +2,5 @@ function clamp(num, min, max){
   return Math.min(Math.max(num, min), max);
 }
 
-console.log(clamp(120, 0, 100)); // 100
+console.log(clamp(120, 0, 200)); // 100
 console.log(clamp(-5, 0, 100));  // 0
